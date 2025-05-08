67b56b40947412fdab74d76b743d16affe57e46b
pielet <syjia0325@gmail.com>
2025-05-08 06:56:12 -0700

    modify dataset generation, add svg example file

--- data/dataset.py
+++ data/dataset.py
@@ -1,25 +1,16 @@
-import os
-
 import numpy as np
-from jax import numpy as jnp
 from torch.utils.data import Dataset
 
 
 class SampleDataset(Dataset):
-    def __init__(self, example_name):
-        """Initialize the dataset with the given example name.
-
-        Args:
-            example_name (str): The name of the example dataset to load.
-        Raises:
-            FileNotFoundError: If the dataset files are not found.
-        """
-        data = np.load(os.path.join('input', f'{example_name}.npy'))
-        self.X = jnp.array(data['X'])
-        self.Y = jnp.array(data['Y'])
+    def __init__(self, dataset_path):
+        data = np.load(dataset_path)
+        self.coordiates = data['X']
+        self.values = data['Y']
+        self.input_dim = self.coordiates.shape[1]
 
     def __len__(self):
-        return len(self.X)
+        return len(self.coordiates)
 
     def __getitem__(self, idx):
-        return self.X[idx], self.Y[idx]
+        return self.coordiates[idx], self.values[idx]
--- data/gen_2d_dataset.py
+++ data/gen_2d_dataset.py
@@ -1,70 +1,62 @@
-import argparse
 import os
 
+import matplotlib.colors as colors
 import matplotlib.pyplot as plt
 import numpy as np
+from omegaconf import OmegaConf
 from sdf import image
+
+from data.shapes import koch_snowflake, sierpinski_triangle
 
 os.makedirs('input', exist_ok=True)
 
 
-def generate_2d_dataset_from_sdf(f, n_samples, min, max):
-    # default scale for all 2d sdf is 1.0, which means their bbox is [0, 0] to [1, 1]
-    X = np.random.uniform(min, max, (n_samples, 2))
+def parse_config(config_path):
+    with open(config_path) as f:
+        config = OmegaConf.load(f)
+    return OmegaConf.to_container(config, resolve=True)
+
+
+def select_sdf(cfg):
+    sdf_fn = {
+        'koch': koch_snowflake,
+        'sierpinski': sierpinski_triangle,
+    }
+
+    if cfg.dataset in sdf_fn.keys():  # fractal sdf
+        f = sdf_fn[cfg.dataset](cfg.order, cfg.scale)
+    elif os.path.exists(f'data/{cfg.dataset}.png'):  # image
+        f = image(f'data/{cfg.dataset}.png', cfg.scale, cfg.scale)
+    else:
+        raise ValueError(f'Unknown dataset: {cfg.dataset}')
+
+    return f
+
+
+def generate_2d_dataset_from_sdf(f, n_samples, min, max, eps=1e-2):
+    X = np.random.uniform(min + eps, max - eps, (n_samples, 2))
     Y = f(X)
     return X, Y
 
 
 def visualize_samples(X, Y, image_path, min, max):
-    plt.figure(figsize=(8, 8))
-    plt.scatter(X[:, 0], X[:, 1], c=Y, cmap='viridis', s=1)
-    plt.colorbar(label='SDF Value')
-    plt.title('2D SDF Samples')
-    plt.xlim(min, max)
-    plt.ylim(min, max)
-    plt.savefig(image_path)
+    fig, ax = plt.subplots(figsize=(8, 7))
+    print(f'min: {np.min(Y)}, max: {np.max(Y)}')
+    norm = colors.TwoSlopeNorm(vmin=np.min(Y), vmax=np.max(Y), vcenter=0)
+    im = ax.scatter(X[:, 0], X[:, 1], c=Y, cmap='RdBu', norm=norm, s=0.1)
+    ax.set_xlim(min, max)
+    ax.set_ylim(min, max)
+    ax.set_aspect('equal', adjustable='box')
+    plt.colorbar(im, ax=ax, fraction=0.04)
+    plt.savefig(image_path, bbox_inches='tight', pad_inches=0.05)
     plt.close()
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generate 2D dataset.')
-    parser.add_argument(
-        '-n',
-        '--n_samples',
-        type=int,
-        required=True,
-        default=1000,
-        help='number of points to sample',
-    )
-    parser.add_argument(
-        '-i', '--image', type=str, help='binary image file path to load the shape from'
-    )
-    parser.add_argument(
-        '-f',
-        '--sdf',
-        choices=['koch_snowflake', 'sierpinski_triangle'],
-        help='ground truth sdf',
-    )
-    parser.add_argument('-o', '--order', type=int, default=0, help='order of the fractal')
-    parser.add_argument('-s', '--scale', type=float, default=1.0, help='scale of the fractal')
-    args = parser.parse_args()
+    import sys
 
-    if hasattr(args, 'image') and args.image is not None:
-        min = -0.5 * args.scale
-        max = 0.5 * args.scale
-        X, Y = generate_2d_dataset_from_sdf(
-            image(args.image, args.scale, args.scale), args.n_samples, min, max
-        )
-        image_name = os.path.splitext(os.path.basename(args.image))[0]
-        visualize_samples(X, Y, f'input/{image_name}_samples.png', min, max)
-        np.savez(f'input/{image_name}.npz', X=X, Y=Y)
-    elif hasattr(args, 'sdf') and args.sdf is not None:
-        min = -0.1 * args.scale
-        max = 1.1 * args.scale
-        X, Y = generate_2d_dataset_from_sdf(
-            globals()[args.sdf](args.order, args.scale), args.n_samples, min, max
-        )
-        visualize_samples(X, Y, f'input/{args.sdf}_samples.png', min, max)
-        np.savez(f'input/{args.sdf}.npz', X=X, Y=Y)
-    else:
-        raise ValueError('Please provide either an image or sdf name.')
+    cfg = parse_config(sys.argv[1])
+    f = select_sdf(cfg)
+    X, Y = generate_2d_dataset_from_sdf(f, cfg.n_samples, cfg.sample_min, cfg.sample_max)
+    np.savez(f'input/{cfg.dataset}.npz', X=X, Y=Y)
+    visualize_samples(X, Y, f'input/{cfg.dataset}_samples.png', cfg.sample_min, cfg.sample_max)
